3b78c08ba02e077598a0d397baa49c2b8160b1ce
Saigesp <saigesp@gmail.com>
2026-03-25 18:35:25 +0100

    add instructions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="app">
     <h1>Maler-<br>femaler</h1>
+    <p class="app__hint">
+      Añade una columna a un archivo CSV con el género de los nombres aportados.
+    </p>
     <div class="app__split">
       <InputText
         v-model="nameColumn"
@@ -63,8 +66,54 @@
         ↺ Reiniciar
       </button>
     </div>
+    <div v-else class="app__instructions">
+      <h3>Instrucciones</h3>
+      <ol>
+        <li>
+          Configura los recuadros superiores:
+          <ul>
+            <li>
+              <strong>Columna nombres:</strong>
+              En qué columna de tu archivo CSV se encuentran los nombres de
+              los que hay que determinar el género.
+            </li>
+            <li>
+              <strong>Nueva columna:</strong>
+              Cómo quieres que se llame la nueva columna.
+            </li>
+            <li>
+              <strong>Clave masculina:</strong>
+              Qué texto pone en la nueva columna a los hombres.
+            </li>
+            <li>
+              <strong>Clave femenina:</strong>
+              Qué texto pone en la nueva columna a las mujeres.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Selecciona en el recuadro grande tu archivo CSV, espera unos instantes
+          y se descargará automáticamente un nuevo archivo con la columna de género añadida.
+        </li>
+      </ol>
+    </div>
     <div class="app__contact">
-      <span>Hecho con ♥️ por <a href="https://github.com/Saigesp">Saigesp</a></span>
+      <p>With ♥️ by <a href="https://github.com/Saigesp">Saigesp</a></p>
+      <a
+        href="https://github.com/Saigesp/maler-femaler"
+        class="app__github-link"
+        aria-label="Contribuye en GitHub">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          fill="currentColor"
+          aria-hidden="true">
+          <!-- eslint-disable-next-line max-len -->
+          <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.385-1.335-1.755-1.335-1.755-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"/>
+        </svg>
+      </a>
     </div>
   </main>
 </template>
@@ -326,6 +375,32 @@ watch(file, async (newFile) => {
     line-height: 1;
   }
 
+  &__hint {
+    color: #5e5a55;
+  }
+
+  &__instructions {
+    color: #5e5a55;
+    margin-top: 2rem;
+    h3 {
+      margin-top: 1.5rem;
+    }
+    ol {
+      list-style-type: decimal;
+      padding-left: 20px;
+      > li {
+        margin-bottom: 0.5rem;
+      }
+    }
+    ul {
+      list-style-type: disc;
+      padding-left: 20px;
+      > li {
+        margin: 0.1rem 0;
+      }
+    }
+  }
+
   &__notfound {
     margin-bottom: 1.25rem;
     border: var(--border);
@@ -364,6 +439,18 @@ watch(file, async (newFile) => {
     &:active {
       box-shadow: none;
       transform: translate(5px, 5px);
+    }
+  }
+
+  &__github-link {
+    display: inline-flex;
+    align-items: center;
+    margin-top: 0.5rem;
+    color: #0a0a0a;
+    transition: opacity 0.15s;
+
+    &:hover {
+      opacity: 0.6;
     }
   }
 
